a644b4b86d651e02d4f472066d2a5e4bc92b3cd2
Jifeng Sun <jsun372@gatech.edu>
2024-04-10 22:54:31 -0400

    Fix Expense Summary page style

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,24 +1,51 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from "../selectors/expenses-total";
 
-export const ExpensesSummary = (props) => (
-    <div>
-        <h1>
-            Viewing {props.expenseCount} expense{props.expenseCount !== 1 ? 's' : ''} totaling {numeral(props.expensesTotal / 100).format('$0,0.00')}
-        </h1>
-    </div>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense': 'expenses' ;
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title"> Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create"> Add Expense</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
-        expenses: visibleExpenses,
         expenseCount: visibleExpenses.length, // Calculate expense count based on filtered expenses
         expensesTotal: getExpensesTotal(visibleExpenses) // Calculate total based on filtered expenses
     };
 };
+
+
+
+// export const ExpensesSummary = (props) => (
+//     <div>
+//         <h1>
+//             Viewing {props.expenseCount} expense{props.expenseCount !== 1 ? 's' : ''} totaling {numeral(props.expensesTotal / 100).format('$0,0.00')}
+//         </h1>
+//     </div>
+// );
+
+// const mapStateToProps = (state) => {
+//     const visibleExpenses = selectExpenses(state.expenses, state.filters);
+//     return {
+//         expenses: visibleExpenses,
+//         expenseCount: visibleExpenses.length, // Calculate expense count based on filtered expenses
+//         expensesTotal: getExpensesTotal(visibleExpenses) // Calculate total based on filtered expenses
+//     };
+// };
 
 export default connect(mapStateToProps)(ExpensesSummary);
